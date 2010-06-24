415da0d9ca1369bba2be42571fb1474d28420fc9
David Hill <DavidPH55@verizon.net>
2010-06-24 15:05:25 -0400

    Rewrote parsing_functions.cpp to use templates instead of macros.

--- DH-dlc/parsing_functions.cpp
+++ DH-dlc/parsing_functions.cpp
@@ -18,7 +18,7 @@
 */
 
 /*
-
+	2010/06/24 - Replaced most of the macros with templates.
 */
 
 #include "parsing.hpp"
@@ -99,133 +99,185 @@ static parsing_FunctionAdder<TYPE##_func_t> \
 parsing_FunctionAdder_##TYPE##_##FUNC##_##NAME \
 (TYPE##_func_map, #NAME, &parse_##TYPE##_function_##FUNC)
 
+#define ADD_FUNCTION2(TYPE1, TYPE2, FUNC, NAME) \
+static parsing_FunctionAdder<TYPE1##_func_t> \
+parsing_FunctionAdder_##TYPE1##_##TYPE2##__##FUNC \
+(TYPE1##_func_map, NAME, &parse_function_##FUNC<TYPE2##_t, to_##TYPE2, parse_##TYPE2>)
+
+#define ADD_FUNCTION(TYPE, FUNC, NAME) \
+ADD_FUNCTION2(TYPE, TYPE, FUNC, NAME)
+
+#define ADD_FUNC_ALL(FUNC, NAME) \
+ADD_FUNC_BOOL  (FUNC, NAME); \
+ADD_FUNC_NUMBER(FUNC, NAME); \
+ADD_FUNC_STRING(FUNC, NAME)
+
+#define ADD_FUNC_BOOL(FUNC, NAME) \
+ADD_FUNCTION(bool,   FUNC, NAME)
+
+#define ADD_FUNC_INT(FUNC, NAME) \
+ADD_FUNCTION(int_s, FUNC, NAME); \
+ADD_FUNCTION(int,   FUNC, NAME); \
+ADD_FUNCTION(int_l, FUNC, NAME); \
+ADD_FUNCTION(sword, FUNC, NAME); \
+ADD_FUNCTION(ubyte, FUNC, NAME); \
+ADD_FUNCTION(uword, FUNC, NAME)
+
+#define ADD_FUNC_NUMBER(FUNC, NAME) \
+ADD_FUNC_INT (FUNC, NAME); \
+ADD_FUNC_REAL(FUNC, NAME)
+
+#define ADD_FUNC_REAL(FUNC, NAME) \
+ADD_FUNCTION(real_s, FUNC, NAME); \
+ADD_FUNCTION(real,   FUNC, NAME); \
+ADD_FUNCTION(real_l, FUNC, NAME)
+
+#define ADD_FUNC_STRING(FUNC, NAME) \
+ADD_FUNCTION(string,   FUNC, NAME); \
+ADD_FUNCTION(string8,  FUNC, NAME)
+
+#define DEFINE_FUNCTION2(TYPE, FUNC) \
+template <class T, T(Tconv)(any_t const &), T(Tparse)(std::string const &)> \
+TYPE parse_function_##FUNC(std::vector<std::string> const & args)
+
+#define DEFINE_FUNCTION(FUNC) \
+DEFINE_FUNCTION2(T, FUNC)
 
 
 
-#define FUNCTION_CMP(TYPE) \
-TYPE##_t parse_##TYPE##_function_cmp(std::vector<std::string> const & args) \
-{ \
-	if (args.size() != 2) \
-		throw FunctionException("2 args required"); \
-	\
-	return to_##TYPE((int_s_t)cmp(parse_##TYPE(args[0]), parse_##TYPE(args[1]))); \
-} \
-ADD_FUNC(TYPE, cmp)
+DEFINE_FUNCTION(cmp)
+{
+	if (args.size() != 2)
+		throw FunctionException("2 args required");
 
-#define FUNCTION_CMP_BOOL(TYPE, LETTER) \
-bool_t parse_bool_function_cmp##LETTER(std::vector<std::string> const & args) \
-{ \
-	if (args.size() != 3) \
-		throw FunctionException("3 args required"); \
-	\
-	int cmpResult = cmp(parse_##TYPE(args[0]), parse_##TYPE(args[2])); \
-	\
-	if (args[1] == cmp_name_eq()) return cmpResult == 0; \
-	if (args[1] == cmp_name_ne()) return cmpResult != 0; \
-	if (args[1] == cmp_name_gt()) return cmpResult >  0; \
-	if (args[1] == cmp_name_ge()) return cmpResult >= 0; \
-	if (args[1] == cmp_name_lt()) return cmpResult <  0; \
-	if (args[1] == cmp_name_le()) return cmpResult <= 0; \
-	\
-	throw FunctionException("unknown cmp:" + args[1]); \
-} \
-ADD_FUNC(bool, cmp##LETTER)
+	return Tconv(int_s_t( cmp(Tparse(args[0]), Tparse(args[1])) ));
+}
+ADD_FUNC_NUMBER(cmp, "cmp");
+ADD_FUNC_STRING(cmp, "cmp");
 
-#define FUNCTION_DISTANCE(TYPE) \
-TYPE##_t parse_##TYPE##_function_distance(std::vector<std::string> const & args) \
-{ \
-	TYPE##_t x1, y1, x2, y2; \
-	\
-	if (args.size() == 2) \
-	{ \
-		obj_t p1 = parse_obj(args[0], type_name_vertex()); \
-		obj_t p2 = parse_obj(args[1], type_name_vertex()); \
-		\
-		x1 = to_##TYPE(p1->getObject(name_t(key_name_x()))); \
-		y1 = to_##TYPE(p1->getObject(name_t(key_name_y()))); \
-		x2 = to_##TYPE(p2->getObject(name_t(key_name_x()))); \
-		y2 = to_##TYPE(p2->getObject(name_t(key_name_y()))); \
-	} \
-	else if (args.size() == 4) \
-	{ \
-		x1 = parse_##TYPE(args[0]); \
-		y1 = parse_##TYPE(args[1]); \
-		x2 = parse_##TYPE(args[2]); \
-		y2 = parse_##TYPE(args[3]); \
-	} \
-	else \
-		throw FunctionException("2 or 4 args required"); \
-	\
-	return hypot(TYPE##_t(x1-x2), TYPE##_t(y1-y2)); \
-} \
-ADD_FUNC(TYPE, distance)
+DEFINE_FUNCTION2(bool_t, bcmp)
+{
+	if (args.size() != 3)
+		throw FunctionException("3 args required");
 
-#define FUNCTION_FACING(TYPE) \
-TYPE##_t parse_##TYPE##_function_facing(std::vector<std::string> const & args) \
-{ \
-	TYPE##_t x1, y1, x2, y2; \
-	\
-	/* ([VERTEX] p1, [VERTEX] p2) */ \
-	if (args.size() == 2) \
-	{ \
-		obj_t p1 = parse_obj(args[0], type_name_vertex()); \
-		obj_t p2 = parse_obj(args[1], type_name_vertex()); \
-		\
-		x1 = to_##TYPE(p1->getObject(name_t(key_name_x()))); \
-		x2 = to_##TYPE(p2->getObject(name_t(key_name_x()))); \
-		\
-		y1 = to_##TYPE(p1->getObject(name_t(key_name_y()))); \
-		y2 = to_##TYPE(p2->getObject(name_t(key_name_y()))); \
-	} \
-	/* ([float] x1, [float] y1, [float] x2, [float] y2) */ \
-	else if (args.size() == 4) \
-	{ \
-		x1 = parse_##TYPE(args[0]); \
-		x2 = parse_##TYPE(args[2]); \
-		\
-		y1 = parse_##TYPE(args[1]); \
-		y2 = parse_##TYPE(args[3]); \
-	} \
-	else \
-		throw FunctionException("2 or 4 args required"); \
-	\
-	TYPE##_t length1; \
-	TYPE##_t length2 = y2 - y1; \
-	TYPE##_t length3 = x2 - x1; \
-	\
-	length1 = hypot(length2, length3); \
-	\
-	/* Not sure what else to do... */ \
-	if (length1 == TYPE##_t(0)) return TYPE##_t(0); \
-	\
-	TYPE##_t angle1 = (TYPE##_t(180) / to_##TYPE(pi())); \
-	TYPE##_t angle2 = asin(TYPE##_t(length2 / length1)) * angle1; \
-	TYPE##_t angle3 = asin(TYPE##_t(length3 / length1)) * angle1; \
-	\
-	/* up-right */ \
-	if (angle2 >= TYPE##_t(0) && angle3 >= TYPE##_t(0)) \
-		return angle2; \
-	\
-	/* up-left */ \
-	else if (angle2 >= TYPE##_t(0) && angle3 < TYPE##_t(0)) \
-		return (TYPE##_t(90) - angle2) + TYPE##_t(90); \
-	\
-	/* down right is +270, left is +180 and +90 for negative */ \
-	else \
-		return angle3 + TYPE##_t(270); \
-} \
-ADD_FUNC(TYPE, facing); \
-ADD_FUNC2(TYPE, facing, angle)
+	int cmpResult = cmp(Tparse(args[0]), Tparse(args[2]));
 
-#define FUNCTION_HYPOT(TYPE) \
-TYPE##_t parse_##TYPE##_function_hypot(std::vector<std::string> const & args) \
-{ \
-	if (args.size() != 2) \
-		throw FunctionException("2 args required"); \
-	\
-	return hypot(parse_##TYPE(args[0]), parse_##TYPE(args[1])); \
-} \
-ADD_FUNC(TYPE, hypot)
+	if (args[1] == cmp_name_eq()) return cmpResult == 0;
+	if (args[1] == cmp_name_ne()) return cmpResult != 0;
+	if (args[1] == cmp_name_gt()) return cmpResult >  0;
+	if (args[1] == cmp_name_ge()) return cmpResult >= 0;
+	if (args[1] == cmp_name_lt()) return cmpResult <  0;
+	if (args[1] == cmp_name_le()) return cmpResult <= 0;
+
+	throw FunctionException("unknown cmp:" + args[1]);
+}
+ADD_FUNCTION2(bool, int_s,  bcmp, "cmpis");
+ADD_FUNCTION2(bool, int,    bcmp, "cmpi");
+ADD_FUNCTION2(bool, int_l,  bcmp, "cmpil");
+ADD_FUNCTION2(bool, real_s, bcmp, "cmpfs");
+ADD_FUNCTION2(bool, real,   bcmp, "cmpf");
+ADD_FUNCTION2(bool, real_l, bcmp, "cmpfl");
+ADD_FUNCTION2(bool, string, bcmp, "cmps");
+
+DEFINE_FUNCTION(distance)
+{
+	T x1, y1, x2, y2;
+
+	if (args.size() == 2)
+	{
+		obj_t p1 = parse_obj(args[0], type_name_vertex());
+		obj_t p2 = parse_obj(args[1], type_name_vertex());
+
+		x1 = Tconv(p1->getObject(name_t(key_name_x())));
+		y1 = Tconv(p1->getObject(name_t(key_name_y())));
+		x2 = Tconv(p2->getObject(name_t(key_name_x())));
+		y2 = Tconv(p2->getObject(name_t(key_name_y())));
+	}
+	else if (args.size() == 4)
+	{
+		x1 = Tparse(args[0]);
+		y1 = Tparse(args[1]);
+		x2 = Tparse(args[2]);
+		y2 = Tparse(args[3]);
+	}
+	else
+		throw FunctionException("2 or 4 args required");
+
+	return hypot(x1 - x2, y1 - y2);
+}
+// TODO: Add hypot for binary ints so this can be ADD_FUNC_NUMBER.
+ADD_FUNCTION(int_s, distance, "distance");
+ADD_FUNCTION(int,   distance, "distance");
+ADD_FUNCTION(int_l, distance, "distance");
+ADD_FUNC_REAL(distance, "distance");
+
+DEFINE_FUNCTION(facing)
+{
+	T x1, y1, x2, y2;
+
+	/* ([VERTEX] p1, [VERTEX] p2) */
+	if (args.size() == 2)
+	{
+		obj_t p1 = parse_obj(args[0], type_name_vertex());
+		obj_t p2 = parse_obj(args[1], type_name_vertex());
+
+		x1 = Tconv(p1->getObject(name_t(key_name_x())));
+		x2 = Tconv(p2->getObject(name_t(key_name_x())));
+
+		y1 = Tconv(p1->getObject(name_t(key_name_y())));
+		y2 = Tconv(p2->getObject(name_t(key_name_y())));
+	}
+	/* ([float] x1, [float] y1, [float] x2, [float] y2) */
+	else if (args.size() == 4)
+	{
+		x1 = Tparse(args[0]);
+		x2 = Tparse(args[2]);
+
+		y1 = Tparse(args[1]);
+		y2 = Tparse(args[3]);
+	}
+	else
+		throw FunctionException("2 or 4 args required");
+
+	T length1;
+	T length2 = y2 - y1;
+	T length3 = x2 - x1;
+
+	length1 = hypot(length2, length3);
+
+	/* Not sure what else to do... */
+	if (length1 == T(0)) return T(0);
+
+	T angle1 = (T(180) / Tconv(pi()));
+	T angle2 = asin(T(length2 / length1)) * angle1;
+	T angle3 = asin(T(length3 / length1)) * angle1;
+
+	/* up-right */
+	if (angle2 >= T(0) && angle3 >= T(0))
+		return angle2;
+
+	/* up-left */
+	else if (angle2 >= T(0) && angle3 < T(0))
+		return (T(90) - angle2) + T(90);
+
+	/* down right is +270, left is +180 and +90 for negative */
+	else
+		return angle3 + T(270);
+}
+ADD_FUNC_REAL(facing, "facing");
+
+DEFINE_FUNCTION(hypot)
+{
+	if (args.size() != 2)
+		throw FunctionException("2 args required");
+
+	return hypot(Tparse(args[0]), Tparse(args[1]));
+}
+// TODO: Add hypot for binary ints so this can be ADD_FUNC_NUMBER.
+ADD_FUNCTION(int_s, hypot, "hypot");
+ADD_FUNCTION(int,   hypot, "hypot");
+ADD_FUNCTION(int_l, hypot, "hypot");
+ADD_FUNC_REAL(hypot, "hypot");
 
 #define FUNCTION_RANDOM(TYPE) \
 TYPE##_t parse_##TYPE##_function_random(std::vector<std::string> const & args) \
@@ -236,44 +288,6 @@ TYPE##_t parse_##TYPE##_function_random(std::vector<std::string> const & args) \
 	return random_##TYPE(parse_##TYPE(args[0]), parse_##TYPE(args[1])); \
 } \
 ADD_FUNC(TYPE, random)
-
-
-
-FUNCTION_CMP(int_s);
-FUNCTION_CMP(int);
-FUNCTION_CMP(int_l);
-FUNCTION_CMP(real_s);
-FUNCTION_CMP(real);
-FUNCTION_CMP(real_l);
-FUNCTION_CMP(string);
-
-FUNCTION_CMP_BOOL(int_s,  is);
-FUNCTION_CMP_BOOL(int,    i);
-FUNCTION_CMP_BOOL(int_l,  il);
-FUNCTION_CMP_BOOL(real_s, fs);
-FUNCTION_CMP_BOOL(real,   f);
-FUNCTION_CMP_BOOL(real_l, fl);
-FUNCTION_CMP_BOOL(string, s);
-ADD_FUNC2(bool, cmpfl, cmp);
-
-FUNCTION_DISTANCE(int_s);
-FUNCTION_DISTANCE(int);
-FUNCTION_DISTANCE(int_l);
-FUNCTION_DISTANCE(real_s);
-FUNCTION_DISTANCE(real);
-FUNCTION_DISTANCE(real_l);
-
-FUNCTION_FACING(real_s);
-FUNCTION_FACING(real);
-FUNCTION_FACING(real_l);
-
-FUNCTION_HYPOT(int_s);
-FUNCTION_HYPOT(int);
-FUNCTION_HYPOT(int_l);
-FUNCTION_HYPOT(real_s);
-FUNCTION_HYPOT(real);
-FUNCTION_HYPOT(real_l);
-
 FUNCTION_RANDOM(int_s);
 FUNCTION_RANDOM(int);
 FUNCTION_RANDOM(int_l);
@@ -283,14 +297,15 @@ FUNCTION_RANDOM(real_l);
 
 
 
-#define PARSE_FUNCTION(TYPE)								\
-TYPE##_t parse_##TYPE##_function(std::string const & opString, std::vector<std::string> const & args)	\
-{											\
-	TYPE##_func_t func = TYPE##_func_map[opString];					\
-											\
-	if (func != NULL) return (*func)(args);						\
-											\
-	throw UnknownFunctionException(opString);					\
+
+#define PARSE_FUNCTION(TYPE) \
+TYPE##_t parse_##TYPE##_function(std::string const & opString, std::vector<std::string> const & args) \
+{ \
+	TYPE##_func_t func = TYPE##_func_map[opString]; \
+	\
+	if (func != NULL) return (*func)(args); \
+	\
+	throw UnknownFunctionException(opString); \
 }
 
 
